709094b9ef50178e39ed248f18acc61362f544c7
MrTesla-Python <63614313+MrTesla-Python@users.noreply.github.com>
2023-07-20 20:32:45 -0400

    Doing it again

--- Module 02/Mod02 Assignments/02.03 Math Class Methods/src/PyTheoremV1.java
+++ Module 02/Mod02 Assignments/02.03 Math Class Methods/src/PyTheoremV1.java
@@ -5,16 +5,16 @@
  * @author Tristan Darnell
  * @version 07/23/23
  */
-public class PyTheorem {
+public class PyTheoremV1 {
     public static void main(String[] args) {
-        double a1 = (int) (Math.random() * 19) + 5;
-        double b1 = (int) (Math.random() * 19) + 5;
-        double c1 = Math.sqrt((Math.pow(a1, 2) + Math.pow(b1, 2)));
+        int a1 = (int) (Math.random() * 18) + 5;
+        int b1 = (int) (Math.random() * 18) + 5;
+        double c1 = Math.sqrt((Math.pow(a1,2) + Math.pow(b1, 2)));
         
         System.out.println("Triangle 1 \t\tSide 1: " + a1 + "\t\t Side 2: " + b1 + "\t\t Hypotenuse: " + c1);
 
-        double a2 = (int) (Math.random() * 19) + 5;
-        double b2 = (int) (Math.random() * 19) + 5;
+        int a2 = (int) (Math.random() * 19) + 5;
+        int b2 = (int) (Math.random() * 19) + 5;
         double c2 = Math.sqrt((Math.pow(a2, 2) + Math.pow(b2, 2)));
         
         System.out.println("Triangle 2 \t\tSide 1: " + a2 + "\t\t Side 2: " + b2 + "\t\t Hypotenuse: " + c2);
